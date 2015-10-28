26909c6f349bda9993112f6e819cf1b05926c378
hbilar <henrik@bilar.co.uk>
2015-10-28 22:17:33 +0000

    Added code to handle microsecond overflows. Changed time stamps to unsigned longs Commented the code a lot more

--- cnc2/stepper.cpp
+++ cnc2/stepper.cpp
@@ -1,26 +1,43 @@
 #include <Arduino.h>
 #include "cnc.h"
 
-volatile long int_counter = 0;
-volatile long last_pulse = 0;  // when was the last step issued
+volatile unsigned long last_pulse = 0;  // when was the last step issued
 
-volatile long steps_left = 0;  // number of steps left in this movement
-volatile char is_moving = 0;   // enables / disables the step generation
+volatile long steps_left = 0;  /* number of steps left in 
+				  this movement */
+volatile char negativeMovement = 0; /* keep track of what 
+				       direction we're moving */
+volatile char is_moving = 0;   /* enables / disables the
+				  step generation */
+
+/* Notes on targetDelay and accelleration values: 
+
+   if maxdelay is larger than targetdelay, then 
+   accelleration / decelleration takes place. 
+     
+   if maxdelay is smaller than targetdelay, no 
+   accelleration / decelleration takes place.
+     
+   To make slow feeds invoke the acc/dec code, increase 
+   maxDelay. To make the accelleration slower/faster
+   decrease/increase the accelleration value.
+*/
+
 
 volatile long targetDelay = 0;
 volatile long curDelay = 0;
 volatile long maxDelay = 1500;
-
 volatile long accelleration = 10;
-volatile long speedLastUpdate = 0;
 
-volatile char negativeMovement = 0;
+/* what microsecond was the speed last updated (acc/dec) */
+volatile unsigned long speedLastUpdate = 0;
 
-/* update speed a maximum of 50 times per second */
+/* update speed a maximum of 500 times per second. Specifies
+ microseconds between speed updates */
 volatile long timeBetweenAccChange = 1000000/500;
 
 
-/* interrupt function to increase int_counter */
+/* interrupt function to move the stepper */
 char movingState = S_STOP;
 volatile long accSteps = 0;
 volatile long decSteps = 0;
@@ -29,25 +46,33 @@ ISR(TIMER2_COMPA_vect)
 {
   /* Check if we're moving. If yes, see if enough time has 
      passed since last pulse, and if yes, pulse again */
-
   if (last_pulse == 0) {
     last_pulse = micros();
   }
 
   if (is_moving) {
-    int_counter++;
-
     /* three states"
        accellerating
        steady speed
        decellerating */
-    long m = micros();
+    unsigned long m = micros();
 
-    if (movingState == S_ACC) {
-      /* recalculate delay if applicable */
+    /* Check if m < last_pulse. If yes, we have overflowed
+       the counter */
+    if (m < last_pulse) {
+      last_pulse = 0; /* This introduces a small error on
+			 overflow, but should be close enough
+			 (max error = 1/50000 s) */
+    }
+
+    /* This section of the code deals with changing the 
+       time between pulses (i.e. accelleration and 
+       decelleration). It could be merged into one 
+       if statement, but they're kept separate to aid 
+       readability at the moment */
+    if (movingState == S_ACC) { /* Accellerating */
       if (curDelay > targetDelay) {
-	/* still accellerating */
-
+	/* We haven't hit target "speed" */
 	/* see if we should accellerate */
 	if ((speedLastUpdate + timeBetweenAccChange) < m) {
 	  /* Timer has expired - lets increase the speed */
@@ -57,15 +82,12 @@ ISR(TIMER2_COMPA_vect)
       } else {
 	/* pin to target delay */
 	curDelay = targetDelay;
-	movingState = S_STEADY;
+	movingState = S_STEADY;  /* record the new state */
       }
     } 
-
-    if (movingState == S_DEC) {
-      /* Decellerating */
+    if (movingState == S_DEC) {  /* Decellerating */
       if (curDelay < targetDelay) {
-	/* still decellarting */
-
+	/* We haven't hit target "speed" */
 	/* see if we should accellerate */
 	if ((speedLastUpdate + timeBetweenAccChange) < m) {
 	  /* Timer has expired - lets decrease the speed */
@@ -78,10 +100,12 @@ ISR(TIMER2_COMPA_vect)
       }
     }
 
-    if (abs(last_pulse - m) > curDelay) {
+    /* This section of the code deals with actually moving
+       the stepper motor */
+    if ((m - last_pulse) > curDelay) {
       last_pulse = m;
       if (steps_left > 0) {
-	/* Move a step */
+ 	/* Move a step */
 	digitalWrite(stepPin, 1);
 	digitalWrite(stepPin, 0);
 	steps_left --;
@@ -91,7 +115,7 @@ ISR(TIMER2_COMPA_vect)
 	  accSteps++;
 	}
 	if (movingState == S_DEC) {
-	  /* record how many steps were spent accellerating */
+	  /* record how many steps were spent decellerating */
 	  decSteps++;
 	}
 	
@@ -104,13 +128,15 @@ ISR(TIMER2_COMPA_vect)
 
 	/* Figure out if we should start decellerating */
 	if (steps_left <= accSteps) {
-	  /* time to start slowing down! */
+	  /* time to start slowing down! This section assumes
+	     that we spend the same amount of time decellerating
+	     (i.e. number of steps) as accellerating. */
 	  movingState = S_DEC;
 	  targetDelay = maxDelay;
 	}
       }
       else {
-	// We've run out of steps - stop moving
+	/* We've run out of steps - stop moving */
 	is_moving = 0;
       }
     }
--- cnc2/stepper.h
+++ cnc2/stepper.h
@@ -1,9 +1,12 @@
 
 
-extern volatile long int_counter;
 extern volatile long steps_left;
-extern volatile long last_pulse;
+extern volatile unsigned long last_pulse;
 extern volatile char is_moving;
 
 extern char Step(long actualSteps, float stepsPerSec);
 extern void EmergencyStop();
+
+
+extern volatile long maxDelay;
+extern volatile long accelleration;
