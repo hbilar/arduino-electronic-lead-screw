0d34da9487eb39f9e46c07886eeafc3b37bbd8ce
hbilar <henrik@bilar.co.uk>
2020-11-16 23:57:05 +0000

    feat: make widgets crop images at the right size feat: add mouse click handler for widgets

---
+++ python-gui/logic.py
@@ -0,0 +1,9 @@
+""" File to handle the logic side of the user interface, e.g. button
+    callbacks """
+
+
+
+def sample_button_callback(widget, pos):
+    """ Sample button callback that just prints stuff to stdout """
+
+    print(f"** Button callback: position = {pos}, widget = {widget}")
