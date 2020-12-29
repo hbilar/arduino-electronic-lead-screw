38bb3b474b0673db4ebe70fbf4fc24b01670c8c8
hbilar <henrik@bilar.co.uk>
2020-12-29 01:09:45 +0000

    doc: flesh out readme.md

--- python-gui/gui-example/hello_gui.py
+++ python-gui/gui-example/hello_gui.py
@@ -67,7 +67,7 @@ def cached_keypad(func):
 
 def example_button_callback(widget, pos):
     """ Example callback for buttons """
-    print(f"EXAMPLE BUTTON PRESSED: {widget}")
+    logging.info(f"EXAMPLE BUTTON PRESSED: {widget}")
 
 
 def main_loop(pyguime_screen, widgets):
@@ -76,15 +76,6 @@ def main_loop(pyguime_screen, widgets):
     running = True
 
     while running:
-
-        # Buttons that are clicked
-        # is_down_widgets = pyguime.find_widgets_by_name(widgets, 'c1')
-        # is_down_widgets = pyguime.find_widgets_by_filter(widgets, lambda x: hasattr(x, 'is_down') and x.is_down)
-        is_down_widgets = pyguime.find_widgets_that_are_down(widgets)
-#        print("####")
-#        for w in is_down_widgets:
-#            print(f"    is_down_widget: {w}")
-
 
         gui_surface.fill((0, 0, 128))
         gui_surface = pyguime.draw_widgets(gui_surface, widgets)
@@ -96,10 +87,10 @@ def main_loop(pyguime_screen, widgets):
 
             # Check keyboard presses
             if event.type in [ pygame.KEYDOWN, pygame.KEYUP ]:
-                print(f"event.key:  {event.key}")
+                logging.debug(f"event.key:  {event.key}")
 
                 if event.key == pygame.K_ESCAPE:
-                    print("Quitting...")
+                    logging.info("Quitting...")
                     running = False
 
             if event.type == pygame.MOUSEBUTTONUP:
@@ -119,104 +110,66 @@ def main():
     # create a screen to draw on
     pyguime_screen = pyguime.setup_screen(width=1024, height=600)
 
-    # widgets - define
+    # Draw a simple textbox on the screen (not inside a container)
+    textbox = pyguime.PyguimeTextbox(text="Random little textbox",
+                                     pos=(10, 200), size=(130, 50),
+                                     font_colour=(255,0,0),
+                                     transparent_background=True)
 
-    keypad_widgets = [ ]
-    key_size = (30, 30)
-    key_space = (10, 5)
-
-    # Set up a textbox and some small squares in a 3x4 pattern
-    keypad_widgets.append(pyguime.PyguimeTextbox(text="Random little widget",
-                                                 pos=(10, 10), size=(100, 50),
-                                                 font_colour=(255,0,0),
-                                                 transparent_background=True))
-    for n in range (0,10):
-        print(f'n = {n}')
-
-        if n == 0:
-            row = 3
-        elif n <= 3:
-            row = 2
-        elif n <= 6:
-            row = 1
-        else:
-            row = 0
-
-        if n == 0:
-            col = 1
-        else:
-            col = (n - 1) % 3
-
-        print(f"  n = {n},   col = {col},  row = {row}")
-
-        keypad_offset = (10, 30)
-        number = pyguime.PyguimeWidget(name=f'num_{n}',
-                                       pos=(keypad_offset[0] +
-                                            col * (key_size[0] +key_space[0]),
-                                            keypad_offset[1] +
-                                            row * (key_size[1] + key_space[1])),
-                                       size=key_size,
-                                       background=(0,20*n,0),
-                                       handle_click_callback=logic.keypad_button_callback
-                                            )
-        keypad_widgets.append(number)
-
-
-    container = pyguime.PyguimeContainer(name="container", pos=(100,300), size=(200,200))
-    container.add(pyguime.PyguimeWidget(name="c1", pos=(10, 20), size=(30, 50), background=(255,0,0)))
-    container.add(pyguime.PyguimeTextbox(name="t1", pos=(50, 0), size=(50, 20), font_size=10, font_colour=(0,255,0)))
-
-    container2 = pyguime.PyguimeContainer(name="container2", pos=(400,300), size=(200,200))
+    container2 = pyguime.PyguimeContainer(name="container2", pos=(300,250), size=(200,200))
     container2.add(pyguime.PyguimeWidget(name="c1", pos=(10, 20), size=(30, 50), background=(128,255,0)))
-    container2.add(pyguime.PyguimeTextbox(name="t1", pos=(50, 0), size=(50, 20), font_size=10, font_colour=(0,0, 255), text="test"))
+    container2.add(pyguime.PyguimeTextbox(name="t1", pos=(50, 0), size=(150, 20), font_size=18, font_colour=(0,0, 255), text="my container"))
 
+    # Keypad for entering decimal numbers
     keypad = pyguime.PyguimeKeypad(name="keypad1", pos=(300, 0), size=(110, 200), background=(50, 50, 50))
     keypad.add_textbox(initial_text="textbox", size=(keypad.size[0], 30),
                        font_size=30, font_colour=(255, 0, 128)).\
         generate(pos=(0, 50), font_colour=(255, 0, 255))
 
-    keypad2 = pyguime.PyguimeKeypad(name="keypad1", pos=(500, 0), size=(110, 200), background=(50, 50, 50))
-    keypad2.add_textbox(initial_text="mytext", size=(keypad2.size[0], 30),
-                        font_size=30). \
-        generate(pos=(0, 50))
+    # A normal button
+    button1 = pyguime.PyguimeButton(name="button1", pos=(450,10), click_callback=example_button_callback,
+                                    text="normal button").generate()
 
-
-    button1 = pyguime.PyguimeButton(name="button1", pos=(600,200), click_callback=example_button_callback).generate()
-    sticky_button1 = pyguime.PyguimeButton(name="sticky_button1", pos=(600,280),
+    # A sticky button that stays pressed when released
+    sticky_button1 = pyguime.PyguimeButton(name="sticky_button1", pos=(450,100),
                                            text="sticky", sticky=True).generate()
 
-    # multi checkbox container with a label
+    # Container with checkboxes with a label
     cb_label = pyguime.PyguimeTextbox(name="mylabel", text="my checkboxes", transparent_background=True).generate()
     checkbox_1 = pyguime.PyguimeCheckbox(name="checkbox1", text="check me").generate()
     checkbox_2 = pyguime.PyguimeCheckbox(name="checkbox2", text="second").generate()
-
-    checkboxes=pyguime.PyguimeContainer(name="checkboxes", pos=(100, 100), auto_size=True, background=(128, 128, 128)).\
+    checkboxes=pyguime.PyguimeContainer(name="checkboxes", pos=(10, 10), auto_size=True, background=(128, 128, 128)).\
         add_object_linear(cb_label).\
         add_object_linear(checkbox_1).\
         add_object_linear(checkbox_2).\
         generate()
-    #text = "check me", sticky = True).generate()
 
-    # radio button
+    # radio buttons
     radio_1 = pyguime.PyguimeCheckbox(name="radio1", text="rad 1", exclusive_group_id="radio1").generate()
     radio_2 = pyguime.PyguimeCheckbox(name="radio1", text="rad 2", exclusive_group_id="radio1", is_down=True).generate()
-
     radio_label = pyguime.PyguimeTextbox(name="mylabelradio", text="my radiobuttons", transparent_background=True).generate()
-    radio_buttons = pyguime.PyguimeContainer(name="radiobuttons", pos=(300, 100), auto_size=True,
+    radio_buttons = pyguime.PyguimeContainer(name="radiobuttons", pos=(150, 10), auto_size=True,
                                              background=(128, 128, 128)). \
         add_object_linear(radio_label). \
         add_object_linear(radio_1). \
         add_object_linear(radio_2). \
         generate()
 
-    widgets = [ pyguime.PyguimeWidget(background=(255,255,0),name="rect1", pos=(500,100), size=(50, 100), click_callback=logic.sample_button_callback),
-                pyguime.PyguimeWidget(name="image", pos=(200, 200), size=(50, 50), image="images/ball.png"),
-                pyguime.PyguimeTextbox(name="keypad_text", pos=(300, 230), size=(100, 100), text='test'),
-                ] + keypad_widgets + [ container ] + [ keypad ] + [ container2] + \
-              [keypad2, button1, sticky_button1, checkboxes, radio_buttons ]
+
+    # yellow rectangle
+    rect1 = pyguime.PyguimeWidget(background=(255,255,0), name="rect1", pos=(10,270), size=(50, 100), click_callback=logic.sample_button_callback)
+
+    # the pygame ball as an image
+    ball = pyguime.PyguimeWidget(name="image", pos=(200, 200), size=(50, 50), image="images/ball.png")
+
+    # make a list of widgets (and define some new widgets...)
+    widgets = [ rect1, ball, textbox , keypad , container2 ,
+               button1, sticky_button1, checkboxes, radio_buttons ]
+
+    # call main loop
     main_loop(pyguime_screen, widgets)
-     
-     
+
+
 if __name__=="__main__":
 
     root_logger = logging.getLogger()
--- python-gui/libraries/README.md
+++ python-gui/libraries/README.md
@@ -1,17 +1,113 @@
 # pyGUIme
 
-Pyguime is a simple GUI framework for pygame.
+Pyguime is a simple GUI framework for [pygame](http://pygame.org).
 
-The framework produces simple graphics, that are suitable
-for use on little Raspberry Pi touchscreens, and
-similar devices.
+The framework can be used to create a *simple* GUI, suitable for
+for use on little Raspberry Pi touchscreens and similar devices. The
+original use case is to create GUIs for simple Arduino projects, but
+other uses are possible.
 
-It is not intended to be a fully fledged GUI replacement, but it
+It is *not* intended to be a fully fledged GUI replacement, but it
 also isn't as complicated to use as some other packages.
+
+
+### Sample widgets
+
+![Sample Widgets](images/sample_pyguime_widgets.png)
+
+
+# Limitations
+
+PyGUIme currently does not handle keyboard input at all. If you have
+keyboard input to handle, please use the normal pygame functionality.
+The reason for this is because the touch screens I play with don't have
+a keyboard. 
+
+Feel free to submit a PR with keyboard callbacks added - it would probably
+be a useful addition!
 
 
 # License
 
 Pyguime is licensed under GNU Lesser General Public License v2.1. See the `LICENSE` file
 for more information.
+
+
+# Why pyGUIme
+
+I have often found myself wishing there was a simple way of driving and
+interacting with a touch screen interface from an Arduino. Whilst possible,
+IMO I do not want to handle this on the Arduino at all - I want the Arduino
+to be focused on handling other hardware I cannot easily interact with
+from Python / Raspberry Pi's etc.
+
+I originally started working on pyGUIme because I wanted to interact with
+my [lathe electronic lead screw](https://github.com/hbilar/arduino-electronic-lead-screw)
+long term project in a way that is difficult and cumbersome on the Arduino
+itself. For my particular project, I need a keypad, various buttons,
+updating text boxes etc. All this could be implemented on a simple LCD panel
+with arrow keys, like I did in my
+[fuel injector tester](https://github.com/hbilar/arduino-injector-tester/blob/master/src/fuel-injector-tester.cpp#L686-L803),
+but it is just painful for anything that is more complicated than the fuel
+injector tester.
+
+## Why pygame?
+
+I love the pygame library. It's simply fantastic for drawing stuff on the
+screen with Python. For doing full screen graphics in Python, I can't think
+of a nicer library (that I know of).
+
+
+# Concepts and examples
+
+## Main loop
+
+The pyGUIme GUI is drawn onto a surface by the `pyguime.draw_widgets` function,
+and events are handled by the `pyguime.handle_mouseclick_up` and
+`pyguime.handle_mouseclick_down` functions. 
+
+All these functions should be called in the appropriate section of the
+main loop in the pygame program.
+
+Pseudo code example:
+
+```pythonstub
+    pyguime_screen = pyguime.setup_screen(width=w, height=height)
+    gui_surface = pygame.Surface((width, height)).convert()
+
+    while running:
+        # draw the pyGUIme widgets / GUI
+        gui_surface = pyguime.draw_widgets(gui_surface, widgets)
+
+        # Handle events (button presses etc)
+        for event in pygame.event.get():
+
+            # Check keyboard presses
+            if event.type == pygame.MOUSEBUTTONUP:
+                pyguime.handle_mouseclick_up(widgets, pygame.mouse.get_pos())
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pyguime.handle_mouseclick_down(widgets, pygame.mouse.get_pos())
+
+        # blit to screen
+        pyguime_screen.screen.blit(gui_surface, (0, 0))
+```
+
+
+## Widgets
+
+The basic object in pyGUIme is a `widget`. A widget is either something
+you can interact with on screen, or a collection of other widgets
+(a container).
+
+The library defines a set of standard widgets, that can be extended, should
+you wish to.
+
+Widgets can have callbacks attached to them, so that when the user clicks
+on e.g. a button (or indeed any `clickable` widget), a user defined function
+is ran.
+
+Widgets have a bunch of properties associated with them. These properties
+are used to determine how to draw the widget, what should happen when it
+is clicked, if it has child widgets inside it, etc etc.
+
 
